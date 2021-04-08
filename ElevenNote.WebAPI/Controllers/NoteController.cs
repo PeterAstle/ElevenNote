a9ed9ec283eafd3a4a3844e6a36c06128ceb6ed4
PeterAstle <pastle32@gmail.com>
2021-04-08 11:39:25 -0400

    Added Put method to NoteController

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -53,5 +53,22 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok(note);
         }
 
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+
     }
 }
