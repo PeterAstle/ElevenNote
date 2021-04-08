0f85242f7755e18796ad408306f23e9cb82578a9
PeterAstle <pastle32@gmail.com>
2021-04-08 11:27:28 -0400

    Added additional methods in NoteServices

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -23,6 +23,7 @@ namespace ElevenNote.Services
             {
                 OwnerId = _userId,
                 Title = model.Title,
+                Content = model.Content,
                 CreatedUtc = DateTimeOffset.Now
             };
 
@@ -55,6 +56,27 @@ namespace ElevenNote.Services
             }
 
 
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
         }
     }
 }
