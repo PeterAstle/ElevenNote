42cd4a261679a200e979747883d5d450df2862a4
PeterAstle <pastle32@gmail.com>
2021-04-12 11:23:56 -0400

    Fixed Category authorization errors

--- ElevenNote.WebAPI/Controllers/CategoryController.cs
+++ ElevenNote.WebAPI/Controllers/CategoryController.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 
 namespace ElevenNote.WebAPI.Controllers
 {
+        [Authorize]
     public class CategoryController : ApiController
     {
         private CategoryService CreateCategoryService()
